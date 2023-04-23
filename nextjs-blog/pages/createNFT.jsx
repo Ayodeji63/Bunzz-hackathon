99a3a73248dd8a9813e31d4d4d689fee39332769
Ayodeji <olusanyasamson633@gmail.com>
2023-04-23 09:41:58 +0100

    createNFT page changes

--- nextjs-blog/pages/createNFT.jsx
+++ nextjs-blog/pages/createNFT.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import CreateNFT from './components/CreateNFT'
-import Header from './components/Header'
+import React from "react"
+import Header from "./components/Header"
+import CreateNFT from "./page/CreateNFT"
 
 const createNFT = () => {
-  return (
-    <div>
-        <Header/>
-        <CreateNFT/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <CreateNFT />
+        </div>
+    )
 }
 
 export default createNFT
